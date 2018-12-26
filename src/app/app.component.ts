5f99a674e50735b1468d6537e11ba908a8f0e2e8
gozdur.sebastian <gozdur.sebastian@gmail.com>
2018-12-26 16:04:46 +0100

    added menu, routings and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'storage-app-front';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  addItem(item) {
+    console.log(item);
+    alert(item);
+  }
+
+  removeItem(id) {
+
+  }
+
+  updateItem(item) {
+
+  }
+
+  getAllItems() {
+    this.http.get("http://localhost:3000/storage/api/v1/items").subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
 }
---
+++ src/app/items-viewer/items-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-viewer',
+  templateUrl: './items-viewer.component.html',
+  styleUrls: ['./items-viewer.component.css']
+})
+export class ItemsViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
